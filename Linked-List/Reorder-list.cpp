d6c43903dd0e49078bb844d21c1008eeeda02271
Rashmi Singh <90052358+Rashmisingh-18@users.noreply.github.com>
2022-07-12 01:01:25 +0530

    Update Reorder-list.cpp

--- Linked-List/Reorder-list.cpp
+++ Linked-List/Reorder-list.cpp
@@ -23,8 +23,8 @@ public:
     }
     
     void reorderList(ListNode* head) {
-	
-        // Finding the middle of list		
+	    
+        // Find the middle of list
         ListNode* slow = head;
         ListNode* fast = head;
         while(fast!=NULL && fast->next!=NULL){
@@ -32,31 +32,30 @@ public:
             fast = fast->next->next;
         }
         
-        // Assigning the head1 as the head of the first halve and 
-        // head2 as the head of the second halve which is slow->next;
-        ListNode* head1 = head;
-        ListNode* head2 = slow->next;
+        ListNode* cur = slow->next;
         slow->next = NULL;
         
-        // Reversing the second half of the list
-        reverseList(head2);
+        ListNode* prev = nullptr, *next = NULL;
         
-        // Alternate merging of the two halves
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-        while(head1!=NULL || head2!=NULL){
-            if(head1){
-                curr->next = head1;
-                curr = curr->next;
-                head1 = head1->next;
-            }
-            if(head2){
-                curr->next = head2;
-                curr = curr->next;
-                head2 = head2->next;
-            }
+        //reverse second part
+        while (cur){
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
         }
-        dummy = dummy->next;
+
+        //merge
+        cur = head;
+        while (cur && prev){
+            next = cur->next;
+            cur->next = prev;
+            cur = next;
+            
+            next = prev->next;
+            prev->next = cur;
+            prev = next;
+	}
     }
 };
 
